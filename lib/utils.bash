ae8b099ec0394e617ec73218b459458c04db9f45
Vlad Tomashpolskyi <vlad.tomashpolskyi@vinted.com>
2023-02-10 14:48:24 +0100

    feat: add govulncheck installation

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for govulncheck.
-GH_REPO="https://github.com/golang/vuln"
+GO_MODULE="golang.org/x/vuln/cmd/govulncheck"
 TOOL_NAME="govulncheck"
 TOOL_TEST="govulncheck --help"
 
@@ -12,40 +11,23 @@ fail() {
   exit 1
 }
 
-curl_opts=(-fsSL)
-
-# NOTE: You might want to remove this if govulncheck is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+list_go_module_versions() {
+  VERSIONS=$(go list -m -versions "$GO_MODULE" | tr ' ' '\n' | sed 's/^v//' | grep -e "\d\+\.\d\+\.\d\+")
+
+  if [ -z "$VERSIONS" ]; then
+    VERSIONS="latest"
+  fi
+
+  echo $VERSIONS
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if govulncheck has other means of determining installable versions.
-  list_github_tags
-}
-
-download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
-
-  # TODO: Adapt the release URL convention for govulncheck
-  url="$GH_REPO/archive/v${version}.tar.gz"
-
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  list_go_module_versions
 }
 
 install_version() {
@@ -58,10 +40,8 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    GOBIN="${install_path}" go install "${GO_MODULE}@${version}"
 
-    # TODO: Assert govulncheck executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
